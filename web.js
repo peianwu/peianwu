2065d23b28b1a95fccd9ec1334f221eaf04f693a
efkztw <peian.peter.wu@gmail.com>
2013-10-04 18:47:33 +0800

    fixed directory target

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express();
 
 app.use(express.logger());
 
-app.use(express.static(path.join(__dirname, "static")));
+app.use(express.static(path.join(__dirname, "public")));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
