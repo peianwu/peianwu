2b206b50d4959737c4abbf05bc58e2225f8b4ce1
Peter Wu <peter.peian.wu@gmail.com>
2013-10-06 19:42:33 +0800

    day 3

---
+++ public/js/around_the_dom.js
@@ -0,0 +1,21 @@
+(function(){
+	window.onload = function() {
+		var windowHeight = window.innerHeight;
+		$(".site-wrapper").css("height",windowHeight);
+		$(".box").css("height",windowHeight * 0.80);
+	};
+
+	$(".here").on("mouseover",function(){
+		var imgSrc = "../assets/img/day2/stickman.jpeg";
+		var imgStick = $("img.stickman");
+		var parentAdd = imgStick.closest(".here");
+
+		imgStick.remove();
+		parentAdd.text("Go Here");
+
+		var direction = $(this).attr("id");
+		
+		var newStick = $(this).text("").append("<img>").find("img");
+		newStick.attr("src",imgSrc).addClass("stickman " + direction + "-rotate");
+	});
+})();
